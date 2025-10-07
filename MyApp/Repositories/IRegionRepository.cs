661fe4d5b2015cadc281c5d0d27bf3c4ba2e0eb1
Yasin <yasinpeker43@gmail.com>
2025-10-07 16:17:46 +0300

    add dependency injection to the repositories

--- MyApp/Repositories/IRegionRepository.cs
+++ MyApp/Repositories/IRegionRepository.cs
@@ -1,8 +1,9 @@
 using System;
+using MyApp.Models;
 
 namespace MyApp.Repositories;
 
 public interface IRegionRepository
 {
-    
+     Task<List<Region>> GetAllAsync();
 }
--- MyApp/Repositories/SQLRegionRepository.cs
+++ MyApp/Repositories/SQLRegionRepository.cs
@@ -1,8 +1,20 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
+using MyApp.Models;
 
 namespace MyApp.Repositories;
 
 public class SQLRegionRepository : IRegionRepository
 {
-   
+   private readonly NZWalkDbContext _regionRepository;
+   public SQLRegionRepository (NZWalkDbContext RegionRepository)
+    {
+        _regionRepository = RegionRepository;
+    }
+    public async Task<List<Region>> GetAllAsync()
+    {
+       return await _regionRepository.Regions.ToListAsync();
+    }
 }
