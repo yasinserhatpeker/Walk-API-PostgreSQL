7a86e2af3403a30cd24538f5c2589341df911311
Yasin <yasinpeker43@gmail.com>
2025-10-09 12:09:19 +0300

    add AutoMapper file

---
+++ MyApp/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+using System;
+using AutoMapper;
+using MyApp.Models;
+using MyApp.Models.DTOs;
+
+namespace MyApp.Mappings;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<Region, RegionDTO>().ReverseMap();
+        CreateMap<AddRegionRequestDTO,Region>().ReverseMap();
+        CreateMap<UpdateRegionRequestDTO, Region>().ReverseMap();
+   }
+}
