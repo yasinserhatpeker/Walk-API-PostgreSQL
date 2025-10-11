a4f89962a50ebe22e6ceca870740c5724e05965e
Yasin <yasinpeker43@gmail.com>
2025-10-11 15:56:09 +0300

    add custom validation method

--- MyApp/CustomActionFilters/ValidateModelAttributes.cs
+++ MyApp/CustomActionFilters/ValidateModelAttributes.cs
@@ -1,9 +1,17 @@
 using System;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace MyApp.CustomActionFilters;
 
 public class ValidateModelAttributes : ActionFilterAttribute
 {
-    
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if(context.ModelState.IsValid == false)
+        {
+            context.Result = new BadRequestResult();
+        }
+    }
 }
