3b093ed88d573e197f16ced02c343db2ca51fa43
Yasin <yasinpeker43@gmail.com>
2025-10-14 08:29:15 +0300

    add the seed Data to NZAuthDb

--- MyApp/Data/NzWalkAuthDbContext.cs
+++ MyApp/Data/NzWalkAuthDbContext.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -7,9 +8,39 @@ namespace MyApp.Data;
 
 public class NzWalkAuthDbContext : IdentityDbContext
 {
-   public NzWalkAuthDbContext(DbContextOptions<NzWalkAuthDbContext> options) : base(options)
+    public NzWalkAuthDbContext(DbContextOptions<NzWalkAuthDbContext> options) : base(options)
     {
-       
+
     }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var readerRoleId = "3f28e4b7-3a69-4a22-b60a-4a7f5877a0b9";
+        var writerRoleId = "c1e2d5a9-77f4-4a9e-bd4a-3de47cb3a831";
+
+        var roles = new List<IdentityRole>
+        {
+           new IdentityRole
+           {
+               Id=readerRoleId,
+              ConcurrencyStamp=readerRoleId,
+              Name="Reader",
+             NormalizedName="Reader".ToUpper()
+           },
+          new IdentityRole
+          {
+              Id=writerRoleId,
+              ConcurrencyStamp=writerRoleId,
+              Name="Writer",
+              NormalizedName="Writer".ToUpper()
+          }
+
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+
+
+    }
+    
 
 }
