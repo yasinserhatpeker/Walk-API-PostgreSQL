8cfffa57ca5c7f549bf9820a1e60422fb24af0ad
Yasin <yasinpeker43@gmail.com>
2025-10-16 12:22:16 +0300

    add useStaticFiles

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MyApp.Data;
@@ -106,6 +107,12 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath="/Images"
+});
 
 app.MapControllers();
 app.Run();
